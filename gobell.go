036ad5be564f88e483a28b1fc75f54e685b7e709
okamura_kenta <side.junktown@gmail.com>
2017-03-19 23:09:47 +0900

    grep dhcpd.lease lines

--- gobell.go
+++ gobell.go
@@ -5,15 +5,12 @@ import (
 	"fmt"
 	"os"
 	"io"
+	"regexp"
 
 	//"github.com/go-fsnotify/fsnotify"
-
-	"io"
 )
 
 func main() {
-
-	fmt.Println("Hello world!")
 
 	fp, err := os.Open("sample/dhcpd.leases")
 	if err != nil {
@@ -21,11 +18,40 @@ func main() {
 	}
 	defer fp.Close()
 
+	// regexp setup
+	rLease    := regexp.MustCompile(`lease [0-9¥.]+ {`)
+    rStarts   := regexp.MustCompile(`starts .*;`)
+    rHwEth    := regexp.MustCompile(`hardware ethernet [0-9A-Fa-f:-]+;`)
+    rHostname := regexp.MustCompile(`client-hostname ".*";`)
+    rEnd      := regexp.MustCompile(`}`)
+
+    // start reading
 	reader := bufio.NewReaderSize(fp, 4096)
-	for line := ""; err == nil; line, err = reader.ReadString('\n') {
-		fmt.Print(line)
-	}
-	if err != io.EOF {
-		panic(err)
+	for {
+		lineBuf, _, err := reader.ReadLine()
+        line := string(lineBuf)
+
+        if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+
+        // try each regexp
+        if rLease.MatchString(line) {
+            fmt.Println("lease {")
+        }
+        if rStarts.MatchString(line) {
+            fmt.Println("start: ")
+        }
+        if rHwEth.MatchString(line) {
+            fmt.Println("hardware ethernet: ")
+        }
+        if rHostname.MatchString(line) {
+            fmt.Println("client-hostname: ")
+        }
+        if rEnd.MatchString(line) {
+            fmt.Println("}")
+        }
 	}
 }
